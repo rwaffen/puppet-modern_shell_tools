b2c6f35df263d51a6c7f2dc0fb7139cd76ecc046
Robert Waffen <rwaffen@gmail.com>
2022-08-17 15:52:56 +0200

    sort init.pp and add viddy

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,10 @@
 class modern_shell_tools {
   include modern_shell_tools::install
   include modern_shell_tools::install::broot
-  include modern_shell_tools::install::lfs
-  include modern_shell_tools::install::exa
-  include modern_shell_tools::install::xsv
   include modern_shell_tools::install::dyff
+  include modern_shell_tools::install::exa
   include modern_shell_tools::install::jc
+  include modern_shell_tools::install::lfs
+  include modern_shell_tools::install::viddy
+  include modern_shell_tools::install::xsv
 }
