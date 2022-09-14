c71f1aa868207efe4121ba70252070611ff74944
Robert Waffen <rwaffen@gmail.com>
2022-09-14 12:59:05 +0200

    fix validate errors

--- spec/classes/modern_shell_tools_spec.rb
+++ spec/classes/modern_shell_tools_spec.rb
@@ -8,20 +8,20 @@ describe 'modern_shell_tools' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
-      it { is_expected.to contain_class('modern_shell_tools::install')}
-      it { is_expected.to contain_class('modern_shell_tools::install::bashtop')}
-      it { is_expected.to contain_class('modern_shell_tools::install::broot')}
-      it { is_expected.to contain_class('modern_shell_tools::install::dyff')}
-      it { is_expected.to contain_class('modern_shell_tools::install::exa')}
-      it { is_expected.to contain_class('modern_shell_tools::install::jc')}
-      it { is_expected.to contain_class('modern_shell_tools::install::jq')}
-      it { is_expected.to contain_class('modern_shell_tools::install::lfs')}
-      it { is_expected.to contain_class('modern_shell_tools::install::viddy')}
-      it { is_expected.to contain_class('modern_shell_tools::install::xsv')}
-      it { is_expected.to contain_file('/opt/mst')}
-      it { is_expected.to contain_package('git')}
-      it { is_expected.to contain_package('unzip')}
-      it { is_expected.to contain_package('wget')}
+      it { is_expected.to contain_class('modern_shell_tools::install') }
+      it { is_expected.to contain_class('modern_shell_tools::install::bashtop') }
+      it { is_expected.to contain_class('modern_shell_tools::install::broot') }
+      it { is_expected.to contain_class('modern_shell_tools::install::dyff') }
+      it { is_expected.to contain_class('modern_shell_tools::install::exa') }
+      it { is_expected.to contain_class('modern_shell_tools::install::jc') }
+      it { is_expected.to contain_class('modern_shell_tools::install::jq') }
+      it { is_expected.to contain_class('modern_shell_tools::install::lfs') }
+      it { is_expected.to contain_class('modern_shell_tools::install::viddy') }
+      it { is_expected.to contain_class('modern_shell_tools::install::xsv') }
+      it { is_expected.to contain_file('/opt/mst') }
+      it { is_expected.to contain_package('git') }
+      it { is_expected.to contain_package('unzip') }
+      it { is_expected.to contain_package('wget') }
     end
   end
 end
